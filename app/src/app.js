16f0489abc5910e6fe0ddb9127c775450c32e5f7
Thana-ptz <154771726+Thana-ptz@users.noreply.github.com>
2024-01-13 03:14:06 +0700

    Create app.js

---
+++ app/src/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const content = document.querySelector('ion-content');
+  content.innerHTML += '<p>This content is dynamically added using JavaScript!</p>';
+});
